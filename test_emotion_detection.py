9a8955e09cb8d0cc38bcc40e8730272d4a74760b
mohamedhassan1222 <ibr.mohamed.hassan@gmail.com>
2026-06-21 16:58:30 +0500

    Update test_emotion_detection.py

--- test_emotion_detection.py
+++ test_emotion_detection.py
@@ -9,11 +9,30 @@ class TestEmotionDetection(unittest.TestCase):
 
     def test_emotion_detector(self):
         """Test dominant emotions for sample statements."""
-        self.assertEqual(emotion_detector("I am glad this happened")["dominant_emotion"], "joy")
-        self.assertEqual(emotion_detector("I am really mad about this")["dominant_emotion"], "anger")
-        self.assertEqual(emotion_detector("I feel disgusted just hearing about this")["dominant_emotion"], "disgust")
-        self.assertEqual(emotion_detector("I am so sad about this")["dominant_emotion"], "sadness")
-        self.assertEqual(emotion_detector("I am really afraid that this will happen")["dominant_emotion"], "fear")
+        self.assertEqual(
+            emotion_detector("I am glad this happened")["dominant_emotion"],
+            "joy"
+        )
+        self.assertEqual(
+            emotion_detector("I am really mad about this")["dominant_emotion"],
+            "anger"
+        )
+        self.assertEqual(
+            emotion_detector("I feel disgusted just hearing about this")[
+                "dominant_emotion"
+            ],
+            "disgust"
+        )
+        self.assertEqual(
+            emotion_detector("I am so sad about this")["dominant_emotion"],
+            "sadness"
+        )
+        self.assertEqual(
+            emotion_detector("I am really afraid that this will happen")[
+                "dominant_emotion"
+            ],
+            "fear"
+        )
 
 
 if __name__ == "__main__":
