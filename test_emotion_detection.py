cf7fb4ba180fe491be8f150c2c98794dfc12c754
mohamedhassan1222 <ibr.mohamed.hassan@gmail.com>
2026-06-21 16:47:28 +0500

    Create test_emotion_detection.py

---
+++ test_emotion_detection.py
@@ -0,0 +1,20 @@
+"""Unit tests for emotion detection application."""
+
+import unittest
+from emotion_detection.emotion_detection import emotion_detector
+
+
+class TestEmotionDetection(unittest.TestCase):
+    """Test cases for emotion_detector function."""
+
+    def test_emotion_detector(self):
+        """Test dominant emotions for sample statements."""
+        self.assertEqual(emotion_detector("I am glad this happened")["dominant_emotion"], "joy")
+        self.assertEqual(emotion_detector("I am really mad about this")["dominant_emotion"], "anger")
+        self.assertEqual(emotion_detector("I feel disgusted just hearing about this")["dominant_emotion"], "disgust")
+        self.assertEqual(emotion_detector("I am so sad about this")["dominant_emotion"], "sadness")
+        self.assertEqual(emotion_detector("I am really afraid that this will happen")["dominant_emotion"], "fear")
+
+
+if __name__ == "__main__":
+    unittest.main()
